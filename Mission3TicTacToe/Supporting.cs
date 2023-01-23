09a347cbc3db20aec849ae1bb852c56e233a4c25
ew11160 <ea11160@byu.edu>
2023-01-23 11:48:34 -0700

    Before debugging the if statement

--- Mission3TicTacToe/Supporting.cs
+++ Mission3TicTacToe/Supporting.cs
@@ -9,60 +9,64 @@ namespace Mission3TicTacToe
         // receive the board array from driver class
         // not sure if this is right. Make sure it receives the right array
         // also check our array numbers
-        string[] arr;
 
         // method to print the board based on information passed
-        public void printBoard()
+        public void printBoard(string[] arr) // on their end they pass us what we need
         {
-            Console.WriteLine(" {0} | {1} | {2} ", arr[1], arr[2], arr[3]);
+            Console.WriteLine(" {0} | {1} | {2} ", arr[0], arr[1], arr[2]); // string parsing grabbing the three arguments following the string
             Console.WriteLine("-----------------");
-            Console.WriteLine(" {0} | {1} | {2} ", arr[4], arr[5], arr[6]);
+            Console.WriteLine(" {0} | {1} | {2} ", arr[3], arr[4], arr[5]);
             Console.WriteLine("-----------------");
-            Console.WriteLine(" {0} | {1} | {2} ", arr[7], arr[8], arr[9]);
+            Console.WriteLine(" {0} | {1} | {2} ", arr[6], arr[7], arr[8]);
         }
 
         // method that receives the game board array as input and returns if there is a winner and who it was
 
         // check for a winner or a draw
-        public string checkWinner()
+        // and end the while loop for the whole program
+        public (string, bool) checkWinner(string[] arr, Boolean winner)
         {
-            if (arr[1] == arr[2] && arr[2] == arr[3])
+            winner = true;
+          if (arr[0] == arr[1] && arr[1] == arr[2])
             {
-                return ("You're the winner!");
+                return ("You're the winner!", winner);
             }
-            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            else if (arr[3] == arr[4] && arr[4] == arr[5])
             {
-                return ("winer");
+                return ("Winner do a funky dance", winner);
             }
-            else if (arr[7] == arr[8] && arr[8] == arr[9])
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
             {
-                return ("winer");
+                return ("Winner take a bow!", winner);
+            }
+            else if (arr[0] == arr[3] && arr[3] == arr[6])
+            {
+                return ("Congratulations, you won!", winner);
             }
             else if (arr[1] == arr[4] && arr[4] == arr[7])
             {
-                return ("winer");
+                return ("Winner winner chicken dinner", winner);
             }
             else if (arr[2] == arr[5] && arr[5] == arr[8])
             {
-                return ("winer");
+                return ("Champion!", winner);
             }
-            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            else if (arr[0] == arr[4] && arr[4] == arr[8])
             {
-                return ("winer");
+                return ("High Five! You won!", winner);
             }
-            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            else if (arr[2] == arr[4] && arr[4] == arr[6])
             {
-                return ("winer");
+                return ("You hit the jackpot!", winner);
             }
-            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            else if (arr[0] != "1" && arr[1] != "2" && arr[2] != "3" && arr[3] != "4" && arr[4] != "5" && arr[5] != "6" && arr[6] != "7" && arr[7] != "8" && arr[8] != "9") ;
             {
-                return ("winer");
+                return ("It's a draw", winner);
             }
-            else if (arr[1] != "1" && arr[2] != "2" && arr[3] != "3" && arr[4] != "4" && arr[5] != "5" && arr[6] != "6" && arr[7] != "7" && arr[8] != "8" && arr[9] != "9") ;
+            else
             {
-                return ("It's a draw");
+                return ("Make your next move", winner = false);
             }
-            // tell the function to keep playing
         }
             
     }
