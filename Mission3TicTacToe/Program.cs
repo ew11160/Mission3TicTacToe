7da6271d0a0ab134bfc8d40ea04cec22746e4fb5
Emma Antonucci <emmanicole0920@gmail.com>
2023-01-23 11:18:28 -0700

    Update Program.cs

--- Mission3TicTacToe/Program.cs
+++ Mission3TicTacToe/Program.cs
@@ -8,26 +8,37 @@ namespace Mission3TicTacToe
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
             Console.WriteLine("Player 1 is X, Player 2 is O");
+            Console.WriteLine();
 
-            int[] choices = new int[9];
-            
-            for (i = 0; i < 9; i++) 
+            string[] choices = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Boolean winner = false;
+            int i = 0;
+            // Call Method Here
+
+            while (winner == false)
             {
                 int position = 0;
+
                 if (i % 2 == 1)
                 {
-                    Console.WriteLine("Player 1, pick a position: ");
-                    position = Console.ReadLine();
+                    Console.WriteLine("Player 2, pick a position: ");
+                    position = Convert.ToInt32(Console.ReadLine());
+
+                    choices[position-1] = "O";
                 }
                 else
                 {
-                    Console.WriteLine("Player 2, pick a position");
-                    position = Console.ReadLine();
-                }
-            }
-            Supporting support = new Supporting(); // naming variable support as a new instance of supporting class
+                    Console.WriteLine("Player 1, pick a position: ");
+                    position = Convert.ToInt32(Console.ReadLine());
 
-            support.printBoard(); // need to compile this
+                    choices[position-1] = "X";
+                }
+
+                i = i + 1; // Incremenent Counter
+
+                // Call Method Here
+            }
         }
     }
+}
 }
