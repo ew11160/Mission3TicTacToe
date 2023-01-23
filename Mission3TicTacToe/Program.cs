fbe7ea45fbae166597880e5bf8709227cf7bb6d0
Emma Antonucci <emmanicole0920@gmail.com>
2023-01-23 08:59:56 -0700

    Update Program.cs

--- Mission3TicTacToe/Program.cs
+++ Mission3TicTacToe/Program.cs
@@ -6,7 +6,25 @@ namespace Mission3TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            Console.WriteLine("Player 1 is X, Player 2 is O");
+
+            int[] choices = new int[9];
+            
+            for (i = 0; i < 9; i++) 
+            {
+                int position = 0;
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("Player 1, pick a position: ");
+                    position = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Player 2, pick a position");
+                    position = Console.ReadLine();
+                }
+            }
         }
     }
 }
