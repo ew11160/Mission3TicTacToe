139ea74d64766b8a75439ba87fc769e931962723
Jackson Huish <jacksonhuish88@gmail.com>
2023-01-23 12:11:13 -0700

    Updates

--- Mission3TicTacToe/Supporting.cs
+++ Mission3TicTacToe/Supporting.cs
@@ -59,7 +59,7 @@ namespace Mission3TicTacToe
             {
                 return ("You hit the jackpot!", winner);
             }
-            else if (arr[0] != "1" && arr[1] != "2" && arr[2] != "3" && arr[3] != "4" && arr[4] != "5" && arr[5] != "6" && arr[6] != "7" && arr[7] != "8" && arr[8] != "9") ;
+            else if (arr[0] != "1" && arr[1] != "2" && arr[2] != "3" && arr[3] != "4" && arr[4] != "5" && arr[5] != "6" && arr[6] != "7" && arr[7] != "8" && arr[8] != "9")
             {
                 return ("It's a draw", winner);
             }
